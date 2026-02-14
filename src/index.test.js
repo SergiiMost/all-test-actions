c56e2eab0f1052cb6e57d186cc56885d22be6b7f
SergiiMost <s.v.mostovoyi@gmail.com>
2026-02-14 16:35:36 -0500

    fix tests

--- src/index.test.js
+++ src/index.test.js
@@ -6,5 +6,5 @@ test('adds 1 + 2 to equal 3', () => {
 
 // This test is intentionally incorrect to demonstrate a failing test case
 test('multiplies 3 * 4 to equal 12', () => {
-  expect(multiply(3, 4)).toBe(11);
+  expect(multiply(3, 4)).toBe(12);
 });
