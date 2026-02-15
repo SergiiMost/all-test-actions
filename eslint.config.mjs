0bf79c33f1938a3515919959858e2cc1e24fa0cf
SergiiMost <s.v.mostovoyi@gmail.com>
2026-02-14 19:58:25 -0500

    Breack lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,9 @@ import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
-function makeReadonlyGlobals(source) {
-  return Object.fromEntries(Object.keys(source).map((k) => [k, 'readonly']));
-}
+// function makeReadonlyGlobals(source) {
+//   return Object.fromEntries(Object.keys(source).map((k) => [k, 'readonly']));
+// }
 
 export default defineConfig([
   {
