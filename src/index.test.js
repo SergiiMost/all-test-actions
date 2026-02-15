1f31d5897c848fbb90408ef00f1795f5f6482089
SergiiMost <s.v.mostovoyi@gmail.com>
2026-02-14 20:02:08 -0500

    test

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-// This test is intentionally incorrect to demonstrate a failing test case
+// This test is intentionally incorrect to demonstrate a failing test case//
 test('multiplies 3 * 4 to equal 12', () => {
   expect(multiply(3, 4)).toBe(12);
 });
